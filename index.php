9adf9e6be2c06b045cf188795fdd82d01ca41859
LUlisesRM <rmilanlu.cue@gmail.com>
2024-05-07 14:05:30 -0600

    Estrcutura del index

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resource/estilo_princial.css">
+    <title>IMAQ</title>
+</head>
+<style> a {text-decoration: none;} </style>
+<body>
+    <header>
+        <div>
+            <div>
+                <img src="resource/media/imaq_mexico.jpg" alt="IMAQ MEXICO">
+            </div>
+
+            <div>
+                <input type="" id="" placeholder="">
+                <button onclick="search()">Buscar</button>
+                <div id="searchResults"></div>
+            </div>
+        </div>
+        
+        <div>
+            <div>
+                <ul>Inicio</ul>
+            </div>
+            <div>
+                <ul>Quienes Somos</ul>
+            </div>
+            <div>
+                <ul>Productos</ul>
+            </div>
+            <div>
+                <ul>Contacto</ul>
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <ul>Crear cuenta</ul>
+            </div>
+            <div>
+                <ul>Ingresar</ul>
+            </div>
+            <div>
+                <ul>Compras</ul>
+            </div>
+        </div>
+</header>
+</body>
+</html>
